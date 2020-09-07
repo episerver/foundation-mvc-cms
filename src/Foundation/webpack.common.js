388251dbd304511f67cdc13a684fa1ef29edf356
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-09-07 23:39:52 +0700

    refactor scss

--- src/Foundation/webpack.common.js
+++ src/Foundation/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
                                 importer: NodeSassGlobImporter()
                             }
                         }
-                    }
+                    },
                 ],
             },
             {
