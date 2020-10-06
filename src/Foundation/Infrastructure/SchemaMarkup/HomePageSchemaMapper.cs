df9202597aa6ffbf444462d9ce131743362860f9
Quoc Anh Nguyen <QuocAnh.Nguyen@episerver.com>
2020-10-06 14:29:55 +0700

    Moved footer to layouts settings

--- src/Foundation/Infrastructure/SchemaMarkup/HomePageSchemaMapper.cs
+++ src/Foundation/Infrastructure/SchemaMarkup/HomePageSchemaMapper.cs
@@ -5,28 +5,38 @@ using Foundation.Features.Home;
 using Schema.NET;
 using System;
 using System.Linq;
+using Foundation.Cms.Settings;
+using Foundation.Features.Settings;
 
 namespace Foundation.Infrastructure.SchemaMarkup
 {
+    
     /// <summary>
     /// Create Schema website and organization objects from HomePage
     /// </summary>
     public class HomePageSchemaMapper : ISchemaDataMapper<HomePage>
     {
+        private readonly ISettingsService _settingsService;
+        public HomePageSchemaMapper(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
         public Thing Map(HomePage content)
         {
+            var layoutSettings = _settingsService.GetSiteSettings<LayoutSettings>();
+
             return new WebSite
             {
                 MainEntity = new Organization
                 {
-                    Name = content.CompanyName ?? content.Name,
+                    Name = layoutSettings?.CompanyName ?? content.Name,
                     Url = SiteDefinition.Current?.SiteUrl,
                     ContactPoint = new ContactPoint()
                     {
-                        Email = content.CompanyEmail ?? new OneOrMany<string>(),
-                        Telephone = content.CompanyPhone ?? new OneOrMany<string>()
+                        Email = layoutSettings?.CompanyEmail ?? new OneOrMany<string>(),
+                        Telephone = layoutSettings?.CompanyPhone ?? new OneOrMany<string>()
                     },
-                    SameAs = content.SocialLinks != null ? new OneOrMany<Uri>(content.SocialLinks.Select(x => new Uri(x.Href ?? string.Empty)).ToArray()) : new OneOrMany<Uri>()
+                    SameAs = layoutSettings?.SocialLinks != null ? new OneOrMany<Uri>(layoutSettings?.SocialLinks.Select(x => new Uri(x.Href ?? string.Empty)).ToArray()) : new OneOrMany<Uri>()
                 },
                 Url = content.GetUri(true)
             };
