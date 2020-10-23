e5d13a55e6bda4bb9c8d70502f8cfbd110062dc0
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2020-10-23 16:40:48 +0700

    Add person item page and list page (#53)

--- src/Foundation/Assets/js/main.js
+++ src/Foundation/Assets/js/main.js
@@ -7,6 +7,7 @@ import "lazysizes/plugins/bgset/ls.bgset";
 import FoundationCms from "Assets/js/common/foundation.cms";
 
 feather.replace();
+window.feather = feather; 
 
 var foundationCms = new FoundationCms();
 foundationCms.init();
