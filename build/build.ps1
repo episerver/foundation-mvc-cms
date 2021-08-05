e38df2339cfaa58b42817148c093e08474663a56
Mark Hall <mark.hall@episerver.com>
2021-08-05 08:50:38 -0700

    Simplify setup

--- build/build.ps1
+++ build/build.ps1
@@ -7,11 +7,7 @@ $ErrorActionPreference = "Stop"
 
 Import-Module .\build\exechelper.ps1
 
-# Install .NET tooling
-
-exec .\build\dotnet-cli-install.ps1
 # Get cli tool
-
 try
 {
     exec "dotnet" "tool install EPiServer.Net.Cli --global --add-source https://pkgs.dev.azure.com/EpiserverEngineering/netCore/_packaging/beta-program/nuget/v3/index.json --version 1.0.0-pre-020034"
